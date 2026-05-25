044dcc3dd385e09e5d054257227f6470f6cbddc2
Appuairtel <124686799+Appuairtel@users.noreply.github.com>
2026-05-25 10:26:51 +0100

    Update app.py
    Smart Race Strategy AI Assistant is an AI-powered solution designed to support race strategy decisions using intelligent data analysis. The system analyses race information such as tyre condition, weather conditions, speed and performance data to generate recommendations for pit stops and race planning.
    
    The goal of this project is to help racing teams and fans better understand race strategies and demonstrate how AI can improve decision-making in real-world situations. The project uses IBM-supported technologies along with Python and Streamlit to create an interactive and practical prototype.

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
 
+import streamlit as st
+import pandas as pd
+
+st.title("🏎 Smart Race Strategy AI Assistant")
+
+uploaded_file = st.file_uploader("Upload race data CSV")
+
+if uploaded_file:
+
+    data = pd.read_csv(uploaded_file)
+
+    st.subheader("Race Data")
+    st.write(data)
+
+    tyre_age = data["TyreAge"].iloc[-1]
+    weather = data["Weather"].iloc[-1]
+
+    st.subheader("AI Recommendation")
+
+    if tyre_age > 4:
+        st.success("Pit stop recommended")
+
+    elif weather == "Rain":
+        st.warning("Switch to wet tyres")
+
+    else:
+        st.info("Continue current strategy")
